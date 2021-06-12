51ad4e5607050034ba3b741f34e6ba2c032d452d
Chal13W1zz <chaliewizz4@gmail.com>
2021-06-12 14:33:08 +0300

    fixed two typos

--- bootloader.asm
+++ bootloader.asm
@@ -9,12 +9,12 @@ msg db "Welcome to Nairobi OS!", 0ah, 0dh, 0h
 
 boot:
    cli ; no interrupts
-   cl4 ; all that is needed to init
+   cld ; all that is needed to init
    hlt ; halt the system
 
 ; we have to be 512 bytes, so we clear the rest of the bytes with 0
 
 times 510 - ($-$$) db 0
 
-dw 0xAA45 ; this is the boot signature
+dw 0xAA55 ; this is the boot signature
 
